16d16a75cb4b42fd81fe3d8e9d80510c194f818f
Kosta <heavyweight@users.noreply.github.com>
2024-11-05 09:17:45 +0100

    Help Center: style archived no results (#96026)
    * HelpCenter: style archived no results
    
    * Add translation domain. Change url
    
    ---------
    
    Co-authored-by: escapemanuele <escapemanuele@gmail.com>

--- packages/help-center/src/components/help-center-chat-history.tsx
+++ packages/help-center/src/components/help-center-chat-history.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-restricted-imports */
 import { HelpCenterSelect } from '@automattic/data-stores';
 import { useSmooch } from '@automattic/zendesk-client';
+import { Card, CardHeader, CardBody } from '@wordpress/components';
 import { useSelect, useDispatch as useDataStoreDispatch } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
+import { comment, Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
+import { Link } from 'react-router-dom';
 import SectionNav from 'calypso/components/section-nav';
 import NavItem from 'calypso/components/section-nav/item';
 import NavTabs from 'calypso/components/section-nav/tabs';
@@ -17,7 +20,11 @@ import './help-center-chat-history.scss';
 const Conversations = ( { conversations }: { conversations: ZendeskConversation[] } ) => {
 	const { __ } = useI18n();
 	if ( ! conversations || ! conversations.length ) {
-		return <div className="help-center-chat-history__no-results">{ __( 'Nothing found…' ) }</div>;
+		return (
+			<div className="help-center-chat-history__no-results">
+				{ __( 'Nothing found…', __i18n_text_domain__ ) }
+			</div>
+		);
 	}
 
 	return (
@@ -73,6 +80,30 @@ export const HelpCenterChatHistory = () => {
 		}
 	}, [ getConversations, isChatLoaded ] );
 
+	const EmptyArchivedConversations = () => {
+		return (
+			<Card isBorderless size="small" className="help-center-chat-history__archive-no-results">
+				<CardHeader className="help-center-chat-history__archive-no-results-header">
+					<h4>{ __( 'Your Archive is Empty', __i18n_text_domain__ ) }</h4>
+				</CardHeader>
+				<CardBody className="help-center-chat-history__archive-no-results-body">
+					{ __(
+						'Resolved issues and past conversations will be available here',
+						__i18n_text_domain__
+					) }
+					<Link
+						to="/odie"
+						onClick={ () => {} }
+						className="help-center-chat-history__archive-no-results-button"
+					>
+						<Icon icon={ comment } />
+						{ __( 'Start conversation', __i18n_text_domain__ ) }
+					</Link>
+				</CardBody>
+			</Card>
+		);
+	};
+
 	return (
 		<div className="help-center-chat-history">
 			<SectionNav>
@@ -82,13 +113,13 @@ export const HelpCenterChatHistory = () => {
 						onClick={ () => setSelectedTab( TAB_STATES.recent ) }
 						count={ unreadCount > 0 ? unreadCount : undefined }
 					>
-						{ __( 'Recent' ) }
+						{ __( 'Recent', __i18n_text_domain__ ) }
 					</NavItem>
 					<NavItem
 						selected={ selectedTab === TAB_STATES.archived }
 						onClick={ () => setSelectedTab( TAB_STATES.archived ) }
 					>
-						{ __( 'Archived' ) }
+						{ __( 'Archived', __i18n_text_domain__ ) }
 					</NavItem>
 				</NavTabs>
 			</SectionNav>
@@ -97,9 +128,12 @@ export const HelpCenterChatHistory = () => {
 				<Conversations conversations={ recentConversations } />
 			) }
 
-			{ selectedTab === TAB_STATES.archived && (
-				<Conversations conversations={ archivedConversations } />
-			) }
+			{ selectedTab === TAB_STATES.archived &&
+				( archivedConversations?.length > 0 ? (
+					<Conversations conversations={ archivedConversations } />
+				) : (
+					<EmptyArchivedConversations />
+				) ) }
 		</div>
 	);
 };
